08d68bfc59f3d99f03518ac04f5f11165f182066
Andre Frolov <aafrolov@ceki.ru>
2020-06-28 16:47:41 +0300

    fix repo path

--- template.rb
+++ template.rb
@@ -78,7 +78,7 @@ def add_template_repository_to_source_path
     at_exit { FileUtils.remove_entry(tempdir) }
     git clone: [
       "--quiet",
-      "https://github.com/mattbrictson/rails-template.git",
+      "https://github.com/frolin/rails-template",
       tempdir
     ].map(&:shellescape).join(" ")
 
